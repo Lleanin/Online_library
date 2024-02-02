fe4eadb1113a3c62a827699350baad8d72a303fc
sssadboy <Fifashf@yandex.ru>
2024-02-02 17:59:06 +0300

    Добавил проверку на наличие страницы

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import requests
+from pathlib import Path
 
 
-for i in range(1, 11):
-    url = f"https://tululu.org/txt.php?id={i}"
+def check_for_redirect(response):
+    if response.history:
+        raise requests.exceptions.HTTPError
 
-    response = requests.get(url)
-    response.raise_for_status()
 
-    filename = f'books/{i}.txt'
-    with open(filename, 'wb') as file:
-        file.write(response.content)
+def main():
+    Path("books").mkdir(parents=True, exist_ok=True)
+    for i in range(1, 11):
+        payload = {
+            "id": i
+        }
+        url = "https://tululu.org/txt.php?"
+
+        try:
+            response = requests.get(url, params=payload)
+            response.raise_for_status()
+            check_for_redirect(response)
+
+            filename = f'books/{i}.txt'
+            with open(filename, 'wb') as file:
+                file.write(response.content)
+        except requests.exceptions.HTTPError:
+            print("Такой страницы не сущетсвует!")
+
+
+if __name__ == '__main__':
+    main()
