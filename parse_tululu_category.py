c49649239b095c1887a16fb6b23f18ac9076edb2
Lleanin <Fifashf@yandex.ru>
2024-03-31 14:17:12 +0300

    added arguments

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,14 +1,33 @@
 import requests
 import json
+import argparse
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from main import parse_book_page, download_image, download_txt
 
 
 def main():
+    parser = argparse.ArgumentParser(
+        description='Данная программа берет книги сайта tululu.org и скачивает их(текст и картинки книг)'
+    )
+    parser.add_argument(
+        '--start_page',
+        help="Начальное id",
+        type=int,
+        default=1
+    )
+    parser.add_argument(
+        '--end_page',
+        help="Конечное id",
+        type=int,
+        default=5
+    )
+    args = parser.parse_args()
+    
+    
     url_template = 'https://tululu.org/l55/'
     book_archive = []
-    for page_number in range(1,5):
+    for page_number in range(args.start_page, args.end_page):
         payload = {
             "id": page_number
             }
