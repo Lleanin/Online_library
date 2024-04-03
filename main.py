36009a025b0a409fcbf0ba14ebd8551ea27674b6
Lleanin <Fifashf@yandex.ru>
2024-04-03 20:13:15 +0300

    added new parameters

--- main.py
+++ main.py
@@ -14,22 +14,26 @@ def check_for_redirect(response):
         raise requests.exceptions.HTTPError
 
 
-def download_txt(response, filename, folder='books/'):
-    Path(folder).mkdir(parents=True, exist_ok=True)
+def download_txt(response, filename, result_folder="Result", folder='books/'):
+    Path(result_folder).mkdir(parents=True, exist_ok=True)
     filename = sanitize_filename(filename)
-    filepath = os.path.join(folder, filename)
+    dest_folder = f"{result_folder}/{folder}"
+    Path(dest_folder).mkdir(parents=True, exist_ok=True)
+    filepath = os.path.join(dest_folder, filename)
     with open(filepath, 'wb') as file:
         file.write(response.content)
 
 
-def download_image(url, folder='images/'):
-    Path(folder).mkdir(parents=True, exist_ok=True)
+def download_image(url, result_folder="Result", folder='images/'):
+    Path(result_folder).mkdir(parents=True, exist_ok=True)
     response = requests.get(url)
     response.raise_for_status()
 
     url = urlparse(url)
     fname = url.path.split("/")[-1]
-    filepath = os.path.join(folder, fname)
+    dest_folder = f"{result_folder}/{folder}"
+    Path(dest_folder).mkdir(parents=True, exist_ok=True)
+    filepath = os.path.join(dest_folder, fname)
 
     with open(filepath, 'wb') as file:
         file.write(response.content)
@@ -65,18 +69,9 @@ def main():
     parser = argparse.ArgumentParser(
         description='Данная программа берет книги сайта tululu.org и скачивает их(текст и картинки книг)'
     )
-    parser.add_argument(
-        '--start_page',
-        help="Начальное id",
-        type=int,
-        default=1
-    )
-    parser.add_argument(
-        '--end_page',
-        help="Конечное id",
-        type=int,
-        default=11
-    )
+    parser.add_argument('--start_page', help="Начальное id", type=int, default=1)
+    parser.add_argument('--end_page', help="Конечное id", type=int, default=11)
+    
     args = parser.parse_args()
 
     for book_number in range(args.start_page, args.end_page):
--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import argparse
+from pathlib import Path
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from main import parse_book_page, download_image, download_txt
@@ -10,18 +11,11 @@ def main():
     parser = argparse.ArgumentParser(
         description='Данная программа берет книги сайта tululu.org и скачивает их(текст и картинки книг)'
     )
-    parser.add_argument(
-        '--start_page',
-        help="Начальное id",
-        type=int,
-        default=1
-    )
-    parser.add_argument(
-        '--end_page',
-        help="Конечное id",
-        type=int,
-        default=5
-    )
+    parser.add_argument('--start_page', help="Начальное id", type=int, default=1)
+    parser.add_argument('--end_page', help="Конечное id", type=int, default=5)
+    parser.add_argument('--skip_imgs', help="Скрипт не скачивает картинки", action='store_false', default=True)
+    parser.add_argument('--skip_txt', help="Скрипт не скачивает книги", action='store_false', default=True)
+    parser.add_argument('--dest_folder',help="Указывает папку в которую будут скачиваться картинки,книги и их параметры", default='Result')
     args = parser.parse_args()
     
     
@@ -49,17 +43,20 @@ def main():
             book_parameters = parse_book_page(page_response)
             book_archive.append(book_parameters)
             
-            
-            filename = f'{book_parameters["name"]}.txt'
-            download_txt(req, filename)
+            if args.skip_txt:
+                filename = f'{book_parameters["name"]}.txt'
+                download_txt(req, filename, args.dest_folder)
 
-            photo_url = urljoin(book_url, book_parameters["book_url"])
-            download_image(photo_url)
+            if args.skip_imgs:
+                photo_url = urljoin(book_url, book_parameters["book_url"])
+                download_image(photo_url, args.dest_folder)
     
     book_archive = json.dumps(book_archive, ensure_ascii=False).encode('utf8')
-    with open("books.json", "w") as my_file:
+    Path(args.dest_folder).mkdir(parents=True, exist_ok=True)
+    folder = f"{args.dest_folder}/books.json"
+    with open(folder, "w") as my_file:
         my_file.write(book_archive.decode())
-
-    
+        
+        
 if __name__ == '__main__':
     main()
